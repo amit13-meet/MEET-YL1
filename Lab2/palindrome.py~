8ccd1ba4b38d921395d83815244b283d744657f9
Amit Nahari <amit13@meet.mit.edu>
2013-10-30 19:55:44 +0200

    DONE (with lab 2)

---
+++ Lab2/palindrome.py~
@@ -0,0 +1,9 @@
+def palindrome():
+	n = raw_input("Give me a word: ")
+	if (n == n[::-1]):
+		return True
+	else:
+		return False
+
+if __name__ == '__main__':
+	print palindrome()
---
+++ Lab2/randomness.py
@@ -0,0 +1,15 @@
+import random
+
+def flip():
+	x = 0 
+	times = raw_input("How many times should I flip the coin? ")
+	while (x < int(times)):
+		n = random.randint(0,1)
+		if (n == 0):
+			print "Heads"
+		else:
+			print "Tails"
+		x += 1
+
+if __name__ == '__main__':
+	flip()
